10511917d4a4b4a3f78da80b1c72c6c267c7acbd
Sa'ad <62077144+BlueZ-95@users.noreply.github.com>
2020-09-16 18:07:20 +0530

    UI updated

--- src/Home.js
+++ src/Home.js
@@ -8,21 +8,51 @@ function Home() {
       <div className="home__container">
         <img
           className="home__image"
-          src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Watches/GW/unrec/Sept/GW_PC_BUNK_watches1500x600._CB405140901_.jpg"
+          src="https://images-eu.ssl-images-amazon.com/images/G/31/prime/Gateway/2020/May/gaming_1500x600._CB431281464_.jpg"
           alt=""
         />
       </div>
       <div className="home__row">
-        <Product />
-        <Product />
+        <Product
+          title="Mathey-Tissot Analog Grey Dial Men's Watch-H1886QPS"
+          price={23600}
+          rating={3}
+          image="https://images-na.ssl-images-amazon.com/images/I/71R5eeQlvHL._UL1440_.jpg"
+        />
+        <Product
+          title="Mathey-Tissot Analog Black Dial Men's Watch-H900ALN"
+          price={11600}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/61Fl4Drk33L._UL1440_.jpg"
+        />
       </div>
       <div className="home__row">
-        <Product />
-        <Product />
-        <Product />
+        <Product
+          title="realme X (Space Blue, 4GB RAM, 128GB Storage)"
+          price={10900}
+          rating={5}
+          image="https://images-na.ssl-images-amazon.com/images/I/81hcorK3VuL._SL1500_.jpg"
+        />
+        <Product
+          title="OnePlus 8 Pro (Onyx Black 8GB RAM+128GB Storage)"
+          price={54999}
+          rating={4}
+          image="https://images-na.ssl-images-amazon.com/images/I/61YSMhOd5EL._SL1500_.jpg"
+        />
+        <Product
+          title="OnePlus 7T Pro (Haze Blue, 8GB RAM, Fluid AMOLED Display)"
+          price={43999}
+          rating={3}
+          image="https://images-na.ssl-images-amazon.com/images/I/61FRLa8IFTL._SL1500_.jpg"
+        />
       </div>
       <div className="home__row">
-        <Product />
+        <Product
+          title="HP Pavilion Gaming 9th Gen Intel Core i5 Processor 15.6-inch FHD Gaming Laptop (8GB/1TB HDD/M.2 Slot/Windows 10/NVIDIA GTX 1650 4GB/Shadow Black), 15-dk0264TX"
+          price={64990}
+          rating={4}
+          image="https://images-na.ssl-images-amazon.com/images/I/81pezrPSgOL._SL1500_.jpg"
+        />
       </div>
     </div>
   );
--- src/Product.js
+++ src/Product.js
@@ -2,23 +2,24 @@
 import React from "react";
 import "./Product.css";
 
-function Product() {
+function Product({ id, title, price, rating, image }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The Lean Startup</p>
+        <p>{title}</p>
         <p className="product__price">
           <small>₹</small>
-          <strong>1499</strong>
+          <strong>{price}</strong>
         </p>
-        <div className="product_rating">
-          <p>⭐</p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
         </div>
       </div>
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
-        alt=""
-      />
+      <img src={image} alt="" />
       <button>Add to Cart</button>
     </div>
   );
