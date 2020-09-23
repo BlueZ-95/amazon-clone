9fb07a00a128e1fd785c525a33fef5c981cfab2b
Sa'ad <62077144+BlueZ-95@users.noreply.github.com>
2020-09-23 17:59:00 +0530

    update

---
+++ src/CheckoutProduct.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./checkoutProduct.css";
+
+function CheckoutProduct({ id, image, title, price, rating }) {
+  return (
+    <div className="checkoutProduct">
+      <img className="checkoutProduct__image" src={image} />
+      <div className="checkoutProduct__info">
+        <p className="checkoutProduct__title">{title}</p>
+        <p className="checkoutProduct__price">{price}</p>
+        <div className="checkoutProduct__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CheckoutProduct;
